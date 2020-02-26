451055f011a9ee79eff22470ccf690390f2005c4
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-26 23:48:09 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -4,5 +4,16 @@ class String
     self.end_with?(".")
   end 
   
+  def question?
+    self.end_with?("?")
+  end 
+  
+  def exclamation?
+    self.end_with?("!")
+  end 
+  
+  def count_sentences
+  end 
+  
   
 end 
